b4528d0882f11ccf75c661bbea660d12586023bc
Jeremiah Dorvil <jeremiahdorvil@outlook.com>
2023-01-31 20:09:18 -0500

    changed local storage

--- script.js
+++ script.js
@@ -4,28 +4,30 @@
 
 $(function () {
     var saveEl = $(".saveBtn");
-    var hour8 = $("#hour-8");
-    var hour9 = $("#hour-9");
-    var hour10 = $("#hour-10");
-    var hour11 = $("#hour-11");
-    var hour12 = $("#hour-12");
-    var hour13 = $("#hour-13");
-    var hour14 = $("#hour-14");
-    var hour15 = $("#hour-15");
-    var hour16 = $("#hour-16");
-    var hour17 = $("#hour-17");
+    // var hour8 = $("#hour-8");
+    // var hour9 = $("#hour-9");
+    // var hour10 = $("#hour-10");
+    // var hour11 = $("#hour-11");
+    // var hour12 = $("#hour-12");
+    // var hour13 = $("#hour-13");
+    // var hour14 = $("#hour-14");
+    // var hour15 = $("#hour-15");
+    // var hour16 = $("#hour-16");
+    // var hour17 = $("#hour-17");
+    // var save
+    // var message = $(".description").text();
     var timeDisplayEl = $('#currentDay')
-    var save
-    var message = $(".description").text();
 
 
     // Added a listener for click events on the save button. 
 
     saveEl.on("click", function(event) {
         event.preventDefault();
-        saveNotesToStorage();
+        var key = $(this).parent().attr("id")
+        var message = $(this).siblings(".description").val()
+        saveNotesToStorage(key, message);
     });
-    save = saveEl.textContent
+    // save = saveEl.textContent
 
     // Added code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour.
@@ -53,31 +55,38 @@ $(function () {
     // Added code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. 
 
-    function saveNotesToStorage() {
-    var notes = {
-        hour: whatHour,
-        comment: message.value
-      };
-    var whatHour = [hour8, hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
-    for(let i = 0; i > whatHour.length; i++)
-    whatHour[i];
+    function saveNotesToStorage(key, val) {
+        localStorage.setItem(key,val)
+    // var notes = {
+    //     hour: whatHour,
+    //     comment: message.value
+    //   };
+    // var whatHour = [hour8, hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+    // for(let i = 0; i > whatHour.length; i++)
+    // whatHour[i];
     
-    if(this !== null){
-      localStorage.setItem("notes", JSON.stringify(notes));
-        } else{
-    return;
-    }};
+    // if(this !== null){
+    //   localStorage.setItem("notes", JSON.stringify(notes));
+    //     } else{
+    // return;
+     };
 
-    function pullNotesFromStorage() {
-        message = localStorage.getItem("notes");
-        if (message) {
-            save = JSON.parse(message);
-        } else {
-            message = [];
-        }
-        return message;
-    };
-    pullNotesFromStorage();          
+    // function pullNotesFromStorage() {
+    //     message = localStorage.getItem("notes");
+    //     if (message) {
+    //         save = JSON.parse(message);
+    //     } else {
+    //         message = [];
+    //     }
+    //     return message;
+    // };
+    // pullNotesFromStorage();  
+    
+    $(".time-block").each(function(){
+        var pull = $(this).attr("id")
+        $(this).children(".description").val(localStorage.getItem(pull))
+    })
+
 
 
     // Added code to display the current date in the header of the page.
